78b4457da471eb31962c582c96d2d1735f268455
Hong Shick Pak <hong@hspak.com>
2015-02-28 22:04:59 -0500

    this is unused now

--- public/js/poll.js
+++
@@ -1,9 +0,0 @@
-console.log("poll loaded");
-setInterval(function() {
-  console.log(document.URL);
-  xmlHttp = new XMLHttpRequest();
-  xmlHttp.open("GET", document.URL + "api/get/data", false);
-  xmlHttp.send(null);
-  // TODO: handle error
-  Data = JSON.parse(xmlHttp.responseText);
-}, 2000);
