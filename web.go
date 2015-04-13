c2279c6ddcf958713927b93c34a94f7998d41b34
Hong Shick Pak <hong@hspak.com>
2015-04-12 22:56:21 -0400

    i dont see this on my server HOW

--- web.go
+++ web.go
@@ -18,11 +18,11 @@ func leadersHandler(w http.ResponseWriter, r *http.Request, db *DB) {
 func serveWeb(db *DB) {
 	fs := http.FileServer(http.Dir("public"))
 	http.Handle("/", fs)
-	http.HandleFunc("/api/get/data", func(w http.ResponseWriter, r *http.Request) {
-		apiHandler(w, r, db)
-	})
 	http.HandleFunc("/api/get/leaders", func(w http.ResponseWriter, r *http.Request) {
 		leadersHandler(w, r, db)
+	})
+	http.HandleFunc("/api/get/data", func(w http.ResponseWriter, r *http.Request) {
+		apiHandler(w, r, db)
 	})
 	log.Println("listening on localhost:4000")
 	http.ListenAndServe(":4000", nil)
