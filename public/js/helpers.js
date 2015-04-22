d632bc1bc3e01b6087fe263234048a85c159d6e1
Hong Shick Pak <hong@hspak.com>
2015-04-22 09:42:52 -0400

    revert

--- public/js/helpers.js
+++ public/js/helpers.js
@@ -1,19 +1,13 @@
 function convertMinutes(minutes, ago) {
-  var ret = "";
   if (minutes == 0) {
-    ret = "now";
+    return "now";
   } else if (minutes < 60) {
-    ret = minutes + "m";
+    return minutes + "m ago";
   } else if (minutes < 1440) { // 60*24
-    ret = Math.round(minutes/60) + "h " + (minutes%60) + "m";
+    return Math.round(minutes/60) + "h " + (minutes%60) + "m ago";
   } else if (minutes < 10080) { // 60*24*7
-    ret = Math.round(minutes/1440) + "d";
+    return Math.round(minutes/1440) + "d ago";
   } else {
-    ret = Math.round(minutes/10080) + "w";
+    return Math.round(minutes/10080) + "w ago";
   }
-
-  if (ago && minutes >= 60) {
-    ret += " ago";
-  }
-  return ret;
 }
